62db2970c3a8298b2279f438880497637e8a97f8
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-08-23 15:03:00 +0200

    fixed .catch fixed state abbreviation

--- js/main.js
+++ js/main.js
@@ -12,6 +12,88 @@ const input= document.querySelector('.search');
 const gridItem= html.children;
 
 
+
+///////////////++++ FUNCTIONS ++++///////////////
+
+function abbrState(input, to){
+
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arizona', 'AZ'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    if (to == 'abbr'){
+        input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        for(i = 0; i < states.length; i++){
+            if(states[i][0] == input){
+                return(states[i][1]);
+            }
+        }
+    } else if (to == 'name'){
+        input = input.toUpperCase();
+        for(i = 0; i < states.length; i++){
+            if(states[i][1] == input){
+                return(states[i][0]);
+            }
+        }
+    }
+}
+
+
+
+
+
+
 ///////////////++++ FETCH ++++///////////////
 
 
@@ -40,8 +122,9 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
     const postcode= data.results[i].location.postcode;
     const dob= data.results[i].dob.date;
     const partOfDob= dob.substring(0, 10);
+    const capitalizeState= `${state.charAt(0).toUpperCase()}${state.substring(1).toLowerCase()}`;
 
-
+    const stateShort= abbrState(state, 'abbr')
 
     html.innerHTML+=
           `<a class="grid-item">
@@ -58,19 +141,21 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
               </div>
               <div class="text-container-modal">
                 <p>${cell}</p>
-                <p>${street} ${postcode} ${state}</p>
-                <p>${partOfDob}</p>
+                <p>${street}, ${stateShort} ${postcode}</p>
+                <p>Birthday: ${partOfDob}</p>
               </div>
           </a>`
   }
 
 
 
-})//****capitalize city****
-
-// .catch(Error=>{
-//   console.log(Error); need to give a better error
-// })
+})
+.catch(Error=>{
+  html.innerHTML = `<div>
+                    <h3>Sorry! Something's not right</h3>
+                    <p> The reason is: ${Error}</p>
+                    </div>`;
+})
 
 
 ///////////////++++ FUNCTIONS ++++///////////////
